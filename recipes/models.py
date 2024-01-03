025028a4e6d4e07a45cf44e9f06370a5c130cc7e
sergionics <sergio27@gmail.com>
2024-01-03 20:29:06 -0300

    criando Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -10,9 +16,18 @@ class Recipe(models.Model):
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
-    preparation_steps_is_htmk = models.BooleanField(default=false)
+    preparation_steps_is_htmk = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
     
