08b1ffbb92b1d346ee43fded237a03e1ee8eeaaf
sergionics <sergio27@gmail.com>
2024-01-10 10:41:41 -0300

    raise http404 in category with django shortcuts

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.http import Http404
@@ -22,18 +22,24 @@ def home(request):
 
 def category(request, category_id):
 
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #    category__id=category_id,
+    #    is_published=True
+    # ).order_by('-id')
 
-    if not recipes:
-        raise Http404('Não encontrado...')
+    # if not recipes:
+    #    raise Http404('Não encontrado...')
+
+    recipes = get_list_or_404( Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
 
     return render(request, "recipes/pages/category.html", context={
         'name': 'Sergionics',
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} -  Category | '
+        'title': f'{recipes[0].category.name} -  Category | '
     })
 
 
