5655076167f774e1354a10b73f0a19635b0dbfd9
sergionics <sergio27@gmail.com>
2024-02-07 17:13:36 -0300

    slug field unique

--- recipes/models.py
+++ recipes/models.py
@@ -12,9 +12,9 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.CharField(max_length=65)
+    preparation_time_unit = models.CharField(max_length=65,default="minutos")
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
