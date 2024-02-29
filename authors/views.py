e354f952f4d30b9563e94c1f0a6a46dea6cc221a
sergionics <sergio27@gmail.com>
2024-02-29 11:12:19 -0300

    initial creation of the user form

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+
+from .forms import RegisterForm
 
 
 # Create your views here.
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    register_form_data = request.session.get('register_form_data', None)
 
+    form = RegisterForm(register_form_data)
+
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+
+    form = RegisterForm(request.POST)
+
+    return redirect('authors:register')
+    
